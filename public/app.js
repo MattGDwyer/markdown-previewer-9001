8732375da1e74a46a2c1fd4d9c02f97235408627
Matthew Dwyer <matthewgdwyer@gmail.com>
2021-12-15 20:04:08 +0700

    changed filetree

---
+++ public/app.js
@@ -0,0 +1,43 @@
+// can i use a plain jsx object somewhere? Maybe the header?
+'use strict';
+
+// components
+const Header = () => {
+  return (
+    <header>
+      {/*beginning to write react dom*/}
+      <h1 className="title">React Markdown Previewer</h1>
+    </header>
+  );
+};
+const Editor = () => {
+  return (
+  <div className="editor">
+  </div>
+    );
+}
+const Preview = () => {
+  return (
+  <div className="preview">
+  </div>
+    );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Body>
+        <Header />
+        <Editor />
+        <Preview />
+      </Body>
+    )
+  }
+};
+
+// Render Function
+ReactDOM.render(<App />, document.getElementById('app'));
