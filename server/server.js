dc534e51d93e9d105d735b5711b7653655961e81
Matthew Dwyer <matthewgdwyer@gmail.com>
2021-12-15 22:08:07 +0700

    further filetree and path adjustments

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", function(req, res) {
-  res.sendFile(`${__dirname}/public/index.html`);
-  }
-);
+// app.get("/", function(req, res) {
+//   res.sendFile(`index.html`);
+//   }
+// );
 
+// app.use(express.static(`${__dirname}/public`));
 app.use(express.static(`${__dirname}/public`));
-app.use("/public", express.static(`${__dirname}~~/public`));
 
 
 
 app.listen(PORT, function() {
-    console.log('Server is running on PORT:',PORT);
+    console.log(`Server is running on PORT:' ${PORT}`);
 });
